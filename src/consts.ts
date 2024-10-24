eab2f5c1c7f16a3f8cb1b1efb5fd2bcc06bf3915
Danira <>
2024-10-24 09:10:08 -0600

    favicon y logo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Beat 100.9 FM';
-export const SITE_DESCRIPTION = 'Total Music';
+export const SITE_TITLE = 'Stereo Cien 100.1';
+export const SITE_DESCRIPTION = 'Siempre Contigo';
