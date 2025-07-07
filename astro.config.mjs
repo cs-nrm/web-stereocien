33a0b8935b1057bad69c0cc3124f35bedeeb3d5c
Csanchez <casanchez@nrmweb.com.mx>
2025-07-07 11:22:22 -0600

    robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stereociendigital.mx',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap({ sitemap: '/sitemap.xml' }), tailwind()],
   devToolbar: {
     enabled: true
   }
