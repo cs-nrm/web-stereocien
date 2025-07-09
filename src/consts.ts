e3cdad538e3065cff772e1675b47b3a9a5c7a5c9
Csanchez <casanchez@nrmweb.com.mx>
2025-07-09 11:26:53 -0600

    metas y comscore

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Stereo Cien 100.1 FM';
-export const SITE_DESCRIPTION = 'Siempre Contigo';
+export const SITE_DESCRIPTION = 'Descubre lo mejor de la música, tendencias, vida, estilo y cultura pop en Stereo Cien. Entérate de noticias, entrevistas y programas exclusivos. ¡Explora y conéctate!';
