4389e97fe9314ff694e3be9bb44a47e1c2241f96
Danira <danira@MacBook-Air-de-Danira.local>
2025-11-05 14:53:11 -0600

    slider buen fin

--- src/js/streaming.js
+++ src/js/streaming.js
@@ -47,6 +47,11 @@ function initGPT() {
     var mapping61 = googletag.sizeMapping().addSize([320, 50]).build();
     var mapping44 = googletag.sizeMapping().addSize([600, 800]).build();
     var mapping441 = googletag.sizeMapping().addSize([300, 400]).build();
+
+    var mapping201 = googletag.sizeMapping().addSize([300, 250]).build();
+    var mapping202 = googletag.sizeMapping().addSize([300, 250]).build();
+    var mapping203 = googletag.sizeMapping().addSize([300, 250]).build();
+    var mapping204 = googletag.sizeMapping().addSize([300, 250]).build();
          
     window.slot2 = googletag.defineSlot("/21799830913/StereoCien", [300, 250],'ad-slot2').defineSizeMapping(mapping2).addService(googletag.pubads());
     window.slot3 = googletag.defineSlot("/21799830913/StereoCien", [728, 90],'ad-slot3').defineSizeMapping(mapping3).addService(googletag.pubads());
@@ -62,6 +67,10 @@ function initGPT() {
     window.slot61 = googletag.defineSlot("/21799830913/StereoCien", [320, 50],'ad-slot61').defineSizeMapping(mapping61).addService(googletag.pubads());
     window.slot44 = googletag.defineSlot("/21799830913/StereoCien", [600, 800],'ad-slot44').defineSizeMapping(mapping44).addService(googletag.pubads());
     window.slot441 = googletag.defineSlot("/21799830913/StereoCien", [300, 400],'ad-slot441').defineSizeMapping(mapping441).addService(googletag.pubads());
+    window.slot201 = googletag.defineSlot("/21799830913/Beat/Box", [300, 250],'ad-slot201').defineSizeMapping(mapping201).addService(googletag.pubads());
+    window.slot202 = googletag.defineSlot("/21799830913/Beat/Box2", [300, 250],'ad-slot202').defineSizeMapping(mapping202).addService(googletag.pubads());
+    window.slot203 = googletag.defineSlot("/21799830913/Beat/Box3", [300, 250],'ad-slot203').defineSizeMapping(mapping203).addService(googletag.pubads());
+    window.slot204 = googletag.defineSlot("/21799830913/Beat/Box4", [300, 250],'ad-slot204').defineSizeMapping(mapping204).addService(googletag.pubads());
 
     googletag.pubads().setTargeting("test","responsive");
     googletag.enableServices();
@@ -79,6 +88,10 @@ function initGPT() {
     googletag.display('ad-slot61');
     googletag.display('ad-slot44');
     googletag.display('ad-slot441');
+    googletag.display('ad-slot201');
+    googletag.display('ad-slot202');
+    googletag.display('ad-slot203');
+    googletag.display('ad-slot204');
     
     //googletag.pubads().refresh([slot3]);
     //setInterval(function(){googletag.pubads().refresh([slot3]);}, 180000);
@@ -102,6 +115,10 @@ function safeRefreshSlots() {
       if (window.slot61) googletag.pubads().refresh([window.slot61]);
       if (window.slot44) googletag.pubads().refresh([window.slot44]);
       if (window.slot441) googletag.pubads().refresh([window.slot441]);
+      if (window.slot201) googletag.pubads().refresh([window.slot201]);
+      if (window.slot202) googletag.pubads().refresh([window.slot202]);
+      if (window.slot203) googletag.pubads().refresh([window.slot203]);
+      if (window.slot204) googletag.pubads().refresh([window.slot204]);
       // O simplemente: googletag.pubads().refresh();
       console.log('Banners refrescados post navegación');
     } else {
@@ -841,6 +858,18 @@ document.addEventListener('astro:page-load', ev => {
             pageDots: false,
             autoPlay: 5000,
         });
+
+        var elembuenfin = document.querySelector('.carousel-buenfin');
+        var flktybuenfin = new Flickity( elembuenfin, {
+            // options
+            cellAlign: 'right',
+            prevNextButtons: true,
+        //    autoPlay: 5000,
+            pageDots: false,
+            pauseAutoPlayOnHover: true,
+            freeScroll: true,
+            wrapAround: true
+        }); 
 
         $('.panel-stereo').click(function(){
             flktypod.resize();
