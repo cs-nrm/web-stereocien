99c21b48c0838a5d6b8c83ad194076aee27dc1d2
Danira <danira@MacBook-Air-de-Danira.local>
2026-02-13 13:41:10 -0600

    Sounds

--- src/js/streaming.js
+++ src/js/streaming.js
@@ -849,7 +849,7 @@ document.addEventListener('astro:page-load', ev => {
         // getInfoProg();
         console.log(getplayingstatus);
 
-        const soundButtons = ['sounds-love', 'sounds-acoustic-moods', 'sounds-retro', 'sounds-soundtracks', 'sounds-covers', 'sounds-jazz-blues', 'sounds-world-music', 'sounds-nu-disco', 'sounds-back-to-disco', 'sounds-latin'];
+        const soundButtons = ['sounds-love', 'sounds-acoustic-moods', 'sounds-retro', 'sounds-soundtracks', 'sounds-covers', 'sounds-jazz-blues', 'sounds-world-music', 'sounds-nu-disco', 'sounds-back-to-disco', 'sounds-latin', 'sounds-live'];
         soundButtons.forEach(buttonId => {
             const element = document.getElementById(buttonId);
             if (element) {
