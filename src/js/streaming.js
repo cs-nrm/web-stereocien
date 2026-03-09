917fbeb7736b82b86e0b80f42cbe478788ab29ee
Danira <danira@MacBook-Air-de-Danira.local>
2026-03-09 13:26:07 -0600

    Imagen de en vivo

--- src/js/streaming.js
+++ src/js/streaming.js
@@ -511,7 +511,7 @@ function getInfoMusic() {
 
 function getInfoProg() {
 
-    fetch("https://stereociendigital.com.mx/wp-json/wp/v2/posts?_embed&per_page=40&categories=302&_fields[]=acf&_fields[]=jetpack_featured_media_url&_fields[]=acf&_fields[]=content")
+    fetch("https://stereociendigital.com.mx/wp-json/wp/v2/posts?_embed&per_page=40&categories=302&_fields[]=_links&_fields[]=_embedded&_fields[]=acf&_fields[]=content")
         .then((res) => {
             if (!res.ok) {
                 throw new Error
@@ -531,7 +531,7 @@ function getInfoProg() {
                 if (prog.acf[dia] === true) {
                     var h_i;
                     if (prog.acf.hora_fin >= hora && prog.acf.hora_inicio <= hora) {
-                        $('.banner-prog img').attr('src', prog.jetpack_featured_media_url);
+                        $('.banner-prog img').attr('src', prog._embedded['wp:featuredmedia'][0].media_details.sizes['full'].source_url);
                         $('.envivo-prog').html(prog.acf.programa);
                         $('.envivo-now').html(prog.acf.hora_inicio + ' - ' + prog.acf.hora_fin);
                         $('.envivo-prog-tab').html(prog.acf.programa);
