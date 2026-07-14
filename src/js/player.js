197a7649615ac27b6c60d7617a44a114e3b04981
Dani <dani@M1.local>
2026-07-14 11:04:40 -0600

    Navidad Sounds

--- src/js/player.js
+++ src/js/player.js
@@ -543,7 +543,7 @@ document.addEventListener('astro:page-load', ev => {
 
         let soundWin = null;
 
-        const soundButtons = ['sounds-love', 'sounds-acoustic-moods', 'sounds-retro', 'sounds-soundtracks', 'sounds-covers', 'sounds-jazz-blues', 'sounds-world-music', 'sounds-nu-disco', 'sounds-back-to-disco', 'sounds-latin', 'sounds-live', 'sounds-fresh'];
+        const soundButtons = ['sounds-love', 'sounds-acoustic-moods', 'sounds-retro', 'sounds-soundtracks', 'sounds-covers', 'sounds-jazz-blues', 'sounds-world-music', 'sounds-nu-disco', 'sounds-back-to-disco', 'sounds-latin', 'sounds-live', 'sounds-fresh', 'sounds-xmas'];
         soundButtons.forEach(buttonId => {
             const element = document.getElementById(buttonId);
             if (element) {
